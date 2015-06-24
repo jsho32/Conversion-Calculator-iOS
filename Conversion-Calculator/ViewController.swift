c0e5773a2be162ec75e4e7b522887b7d735042b9
jsho32 <justin.shores32@gmail.com>
2015-06-23 23:42:43 -0700

    Create initial project.

---
+++ Conversion-Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Conversion-Calculator
+//
+//  Created by Justin Shores on 6/23/15.
+//  Copyright © 2015 Justin Shores. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
